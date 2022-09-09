0feb3d9056441ebc7f4e9e1a6631a5517d1c4185
Tobias Knipping <t.knipping@tks.eu>
2022-09-09 11:53:38 +0200

    add possibility to define snmp timeout/retries

--- check_synology.py
+++ check_synology.py
@@ -19,6 +19,8 @@ parser.add_argument("mode", help="the mode", type=str, choices=["load", "memory"
 parser.add_argument("-w", help="warning value for selected mode", type=int)
 parser.add_argument("-c", help="critical value for selected mode", type=int)
 parser.add_argument("-p", help="the snmp port", type=int, dest="port", default=161)
+parser.add_argument("--timeout", help="timeout for snmp connection", type=int, default=30)
+parser.add_argument("--retries", help="retries for snmp connection if timeout occurs", type=int, default=1)
 args = parser.parse_args()
 
 hostname = args.hostname
@@ -29,6 +31,8 @@ priv_key = args.privkey
 mode = args.mode
 warning = args.w
 critical = args.c
+snmp_timeout = args.timeout
+snmp_retries = args.retries
 
 state = 'OK'
 
@@ -46,6 +50,8 @@ try:
     session = easysnmp.Session(
         hostname=hostname,
         version=3,
+        timeout=snmp_timeout,
+        retries=snmp_retries,
         security_level="auth_with_privacy",
         security_username=user_name,
         auth_password=auth_key,
