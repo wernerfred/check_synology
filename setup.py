4dc4789c62be93d7b493fe9c9f2619d492e0087e
fibberblank <68998809+fibberblank@users.noreply.github.com>
2023-05-04 08:54:19 +0200

    Update setup.py
    Missing suffix

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     license="AGPL-3.0",
     keywords="synology, synology-diskstation, snmp, snmpv3, monitoring, monitoring-plugin, nagios, icinga2, icinga2-plugin",
     py_modules=["check_synology"],
-    scripts=["check_synology"],
+    scripts=["check_synology.py"],
     python_requires=">=3.4",
     install_requires=["easysnmp>=0.2.6,<1"],
     classifiers=[
